f2990d6971975b4056614b333d4614f46cdafc53
yus04 <yuswim192@gmail.com>
2025-03-05 16:57:26 +0900

    update AI Document Intelligence file in with-apim

--- with-apim/azure-ai-document-intelligence.py
+++ with-apim/azure-ai-document-intelligence.py
@@ -10,11 +10,9 @@ apim_base_url_for_ai_document_intelligence = os.getenv("APIM_BASE_URL_FOR_AI_DOC
 ai_document_intelligence_api_key = os.getenv("AI_DOCUMENT_INTELLIGENCE_API_KEY")
 ocp_apim_subscription_key = os.getenv("OCP_APIM_SUBSCRIPTION_KEY")
 
-# 通常は以下のように AzureKeyCredential を使って認証情報を設定
-# credential = AzureKeyCredential(ai_document_intelligence_api_key)
-
-# ただし、APIM を経由する場合は以下のように AzureKeyCredential を使って認証情報を設定
-credential = AzureKeyCredential(ocp_apim_subscription_key)
+# 以下のように AzureKeyCredential で設定した認証情報は、
+# リクエスト時に Ocp-Apim-Subscription-Key ヘッダーとして付与される
+credential = AzureKeyCredential(ai_document_intelligence_api_key)
 
 # 以下を変更してください
 document_name = "sample.pdf"
@@ -31,10 +29,10 @@ with open(path_to_sample_documents, "rb") as f:
     poller = ai_document_intelligence_client.begin_analyze_document(
         "prebuilt-layout",
         body = f,
-        # 以下のようにヘッダーを指定することはできない (credential で上書きされる)
-        # headers = {
-        #     "Ocp-Apim-Subscription-Key": ocp_apim_subscription_key
-        # }
+        # APIM 認証用のサブスクリプションキー設定
+        headers = {
+            "Ocp-Apim-Subscription-Key-Added": ocp_apim_subscription_key
+        }
     )
 
 result: AnalyzeResult = poller.result()
